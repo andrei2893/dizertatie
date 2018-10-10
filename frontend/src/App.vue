dbeb6706530ce929ee883dda3d463fda02ee4a49
Andrei Paul <andreipaulalbu@gmail.com>
2018-10-10 20:03:27 +0300

    header&login

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+    <Login/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from '@/components/Header.vue';
+import Login from '@/components/Login.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header,
+    Login,
   }
 }
 </script>
@@ -19,10 +21,9 @@ export default {
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+body {
+  margin: 0;
 }
 </style>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="header">
+        <img class="logo" src="pictures/bee.png">
+         <img class="logo" src="pictures/bee-live.png">
+        <img class="logo" src="pictures/bee.png">
+       
+    </div>
+</template>
+
+<script>
+export default {
+
+
+}
+</script>
+
+<style scoped>
+.header {
+    width: 100%;
+    height: 80px;
+    background-color:black;
+    display: flex;  
+    justify-content: space-between;
+}
+
+.logo {
+    height: 100%;
+}
+</style>
