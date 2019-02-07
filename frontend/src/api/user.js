088e0b224ae3feb08ee738dea19624ea0aa0456d
Andrei Paul <andreipaulalbu@gmail.com>
2019-02-07 17:32:51 +0200

    stergere useri

--- frontend/src/api/user.js
+++ frontend/src/api/user.js
@@ -37,3 +37,16 @@ export function registerClerk(dto, credentials) {
     const body = JSON.stringify(dto)
     return fetch(new Request('/api/auth/user/admin'), init('POST', headers, body)).then((response) => response.ok);
 }
+
+export function findAllUsers() {
+    const headers = makeHeaders()
+    headers.append('Authorization', getCredentials())
+    return fetch(new Request('/api/auth/user/all'), init('GET', headers)).then((response) => response.json());
+}
+
+export function deleteAllUsers(userIds) {
+    const headers = makeHeaders()
+    headers.append('Authorization', getCredentials())
+    const ids = userIds.reduce((acc, val) => acc + ',' + val)
+    return fetch(new Request('/api/auth/user/all/delete?ids=' + ids), init('DELETE', headers)).then((response) => response.ok);
+}
--- frontend/src/components/admin/AdminDeleteUser.vue
+++ frontend/src/components/admin/AdminDeleteUser.vue
@@ -1,10 +1,48 @@
 <template>
-    
+    <div>
+        <md-list>
+            <md-list-item v-for="user of users" :key="user.id">
+                <md-checkbox v-model="idsToBeDeleted" :value="user.id">
+                    {{user.name}}
+                </md-checkbox>
+                <span>
+                    {{user.email}}
+                </span>
+            </md-list-item>
+            <md-list-item>
+                <md-button class="md-primary" @click="deleteUsers" :disabled="!idsToBeDeleted.length">
+                    delete
+                    </md-button>
+            </md-list-item>
+        </md-list>
+    </div>
 </template>
 
 <script>
+import {findAllUsers,deleteAllUsers} from '@/api/user'
 export default {
+    data() {
+        return {
+            users: [],
+            idsToBeDeleted: [],
+        }
+    },
+    mounted() {
+        findAllUsers().then((users) => this.users = users)
+    },
+    methods: {
+        deleteUsers() {
+            deleteAllUsers(this.idsToBeDeleted).then((ok) => {
+                if (ok) {
+                    findAllUsers().then((users) => {
+                        this.users = users
+                        this.idsToBeDeleted = []
+                    })
 
+                }
+            })
+        }
+    }
 }
 </script>
 
